3b4c084a65f6848a4e7dc61adc09bd018859d6ef
Harshpateln01351133 <n01351133@humbermail.ca>
2021-02-22 16:17:56 -0500

    Add README.md file to github

---
+++ HarshAssign22/src/main/java/harsh/patel/n01351133/HarshActivity.java
@@ -0,0 +1,15 @@
+//harsh patel,n01351133 section c
+package harsh.patel.n01351133;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class HarshActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
---
+++ README.md
@@ -0,0 +1,3 @@
+# Harsh Patel N01351133 section c
+## https://github.com/harshpateln01351133/HarshPatelAssign2
+### This application allows to choose pizza restaurant and order pizza from restaurant.
